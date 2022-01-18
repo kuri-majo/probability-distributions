0f5998a8934697d2a8e2321a645c18610023a96d
I.K <tessier_ashpool@outlook.com>
2022-01-18 20:54:30 +0100

    first plot

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from scipy.stats import uniform
+import seaborn as sns
 
+# random numbers from uniform distribution
+n = 10000
+start = 10
+width = 20
+data_uniform = uniform.rvs(size=n, loc = start, scale=width)
+
+ax = sns.distplot(data_uniform,
+                  bins=100,
+                  kde=True,
+                  color='skyblue',
+                  hist_kws={"linewidth": 15,'alpha':1})
+ax.set(xlabel='Uniform Distribution ', ylabel='Frequency')
