352e9155bf55c820587e8c7444601fda8826f8dd
I.K <tessier_ashpool@outlook.com>
2022-01-22 17:24:26 +0100

    changed to displot because of deprecation warning

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math import gamma
+from turtle import title
 from scipy.stats import uniform
 import seaborn as sns
 
@@ -7,9 +9,12 @@ start = 10
 width = 20
 data_uniform = uniform.rvs(size=n, loc = start, scale=width)
 
-ax = sns.distplot(data_uniform,
-                  bins=100,
-                  kde=True,
-                  color='skyblue',
-                  hist_kws={"linewidth": 15,'alpha':1})
-ax.set(xlabel='Uniform Distribution ', ylabel='Frequency')
+g = sns.displot(
+    data=data_uniform,
+    bins=20,
+    stat="proportion",
+    kde=True,
+    palette="crest",
+    line_kws={"linewidth": 4},
+    )
+g.set(title="Uniform Distribution")
